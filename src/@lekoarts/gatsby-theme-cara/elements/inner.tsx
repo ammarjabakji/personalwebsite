c2c14f5984d183efc7d32bd07832f1f7c57e2695
Ammar Jabakji <ammarjabakji@gmail.com>
2019-10-31 11:26:15 +0300

    changes some files

---
+++ src/@lekoarts/gatsby-theme-cara/elements/inner.tsx
@@ -0,0 +1,23 @@
+/** @jsx jsx */
+import React from "react";
+import { jsx } from "theme-ui";
+
+const Inner = ({
+  className,
+  children
+}: {
+  className?: string;
+  children?: React.ReactNode;
+}) => (
+  <div
+    className={className}
+    sx={{
+      width: [`full`, `full`, `full`, `full`, `full`, `full`],
+      textAlign: `left`
+    }}
+  >
+    {children}
+  </div>
+);
+
+export default Inner;
---
+++ src/pages/education.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const ContactPage = () => (
+  <main>
+    <h1>Contact us</h1>
+    <p>Contact informations</p>
+  </main>
+);
+export default ContactPage;
